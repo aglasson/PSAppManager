d37d02a8a72ee720954d54aa0811ac08a2cfee52
Adrian <adrian@glassons.id.au>
2019-10-16 11:23:23 +1000

    Adding VSCode launch json for ease of debugging

---
+++ .vscode/SetupDebugModule.ps1
@@ -0,0 +1,8 @@
+try {
+    Clear-Host
+    Import-Module .\PSAppManager.psd1 -Force -ErrorAction Stop
+    Write-Host "PSAppsManager Powershell module re-imported in session for debugging"
+}
+catch {
+    Write-Error "Unable to import module with VSCode launch script"
+}
