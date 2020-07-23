a88f629e4380f3641ba2c40d6e5faba394ac98b7
Adrian <adrian@glassons.id.au>
2020-07-23 19:24:49 +1000

    Imporoved vscode debug information output

--- .vscode/SetupDebugModule.ps1
+++ .vscode/SetupDebugModule.ps1
@@ -1,8 +1,8 @@
 try {
     Clear-Host
     Import-Module .\PSAppManager\PSAppManager.psd1 -Force -ErrorAction Stop
-    Write-Host "PSAppsManager Powershell module re-imported in session for debugging"
+    Write-Host "PSAppsManager Powershell module re-imported in session for debugging - Available Commands: $((Get-Module PSAppManager).ExportedCommands.Keys)"
 }
 catch {
-    Write-Error "Unable to import module with VSCode launch script"
+    Write-Error -Message "Unable to import module with VSCode launch script: $($_.Exception)"
 }
